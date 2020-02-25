dca8bd646ab54949c83de881f49fa8a8b2deb1ff
Karl Beier <karl.beier@optus.com.au>
2020-02-26 07:16:39 +1100

    Add display of loot frame

--- HugeLoot.lua
+++ HugeLoot.lua
@@ -1,10 +1,13 @@
 HugeLoot = LibStub("AceAddon-3.0"):NewAddon("HugeLoot", "AceConsole-3.0")
 local LDB = LibStub("LibDataBroker-1.1", true)
 local LDBIcon = LibStub("LibDBIcon-1.0", true)
-local channelToMasterLoot = "RAID_WARNING"
-local channelToAnnounce = "RAID"
-local itemsToNotAnnounce = {
-	["Lava Core"] = true,
+local AceGUI = LibStub("AceGUI-3.0")
+
+local CHANNEL_TO_MASTER_LOOT = "RAID_WARNING"
+local CHANNEL_TO_ANNOUNCE = "RAID"
+local DEFAULT_PRIORITY = "MS > OS"
+local ITEMS_TO_NOT_ANNOUNCE = {
+	["Lava Core"] = true,	
 	["Fiery Core"] = true,
 	["Elementium Ore"] = true
 }
@@ -18,7 +21,7 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("LOOT_OPENED") -- Fired on user opening a loot panel
 frame:SetScript("OnEvent", function(self, event, ...)
 	if event == "LOOT_OPENED" then 
-		ProcessLoot()
+		processLoot()
 	end
 end)
 
@@ -37,6 +40,7 @@ local configOptions = {
 			desc = "Minimum rarity for master looting",
 			type = "select",
 			values = {
+				[1] = "Common", --TODO remove
 				[2] = "Uncommon",
 				[3] = "Rare",
 				[4] = "Epic"
@@ -78,7 +82,6 @@ local defaults = {
 }
 
 function HugeLoot:OnInitialize()
-	HugeLoot:Print("initialized")
   	self.db = LibStub("AceDB-3.0"):New("HugeLootDB", defaults)
 	self.db.RegisterCallback(self, "OnProfileChanged", "RefreshConfig")
 	self.db.RegisterCallback(self, "OnProfileCopied", "RefreshConfig")
@@ -149,23 +152,22 @@ function toggleConfigWindow()
 end
 
 function parseLootPriority(input) 
+	local itemsParsed = 0
 	for line in input:gmatch("[^\r\n]+") do
-		--HugeLoot:Print(""..line)
 		local _, name, priority, note, _ = line:match("%s*(.-),%s*(.-),%s*(.-),%s*(.-),%s*(.-)")
-		
-		--HugeLoot:Print("Name: "..name)
-		--HugeLoot:Print("Priority: "..priority)
-		--HugeLoot:Print("Note: "..note)
+
 		prioritySet[name] = {
 			["priority"] = priority,
 			["note"] = note
 		}
+		itemsParsed = itemsParsed + 1
 	end
 	HugeLoot.db.profile.prioritySet = prioritySet
-	HugeLoot:Print(prioritySet)
+	HugeLoot:Print("Successfully added "..itemsParsed.." items to the priority database")
 end
 
-function ProcessLoot() 
+function processLoot() 
+	--HugeLoot:Print("Processing loot")
 	local masterlooterRaidID = select(3, GetLootMethod())
 	local isMasterLooter = masterlooterRaidID ~= nil and UnitName("raid"..masterlooterRaidID) == UnitName("player")
 	local guid =  UnitGUID("target")
@@ -176,33 +178,114 @@ function ProcessLoot()
 	-- Build a list of links
 	local lootMessage = ""
 	local currentLoot = GetLootInfo()
+	local masterLootCandidates = {}
+	local numItemsToMasterLoot = 0
 	for i = 1, #currentLoot do
 		local _, name, _, _, quality = GetLootSlotInfo(i)
-		--local name = select(2, GetLootSlotInfo(i))
-		--local quality = select(5, GetLootSlotInfo(i))
+		local link = GetLootSlotLink(i)
+		--HugeLoot:Print("Processing item "..i.." with name "..name)
 		
-		if not(itemsToNotAnnounce[name]) and quality >= minItemQualityToAnnounce then
-			lootMessage = lootMessage .. GetLootSlotLink(i)
+		if not(ITEMS_TO_NOT_ANNOUNCE[name]) and quality >= minItemQualityToAnnounce and link ~= nil then
+			lootMessage = lootMessage..link
+		end
+		
+		if quality >= minItemQualityToMasterLoot then 
+			numItemsToMasterLoot = numItemsToMasterLoot + 1
+			local priorityEntry = prioritySet[name]
+			
+			if priorityEntry ~= nil then 					
+				masterLootCandidates[name] = {
+					["link"] = link,
+					["priority"] = priorityEntry.priority.." > "..DEFAULT_PRIORITY,
+					["note"] = priorityEntry.note
+				}
+			else
+				local specTable = {}--GetItemSpecInfo(name)
+				if #specTable >= 1 then 
+					masterLootCandidates[name] = {
+						["link"] = link,
+						["priority"] = specTable[0],
+						["note"] = ""
+					}
+				else 
+					masterLootCandidates[name] = {
+						["link"] = link,
+						["priority"] = DEFAULT_PRIORITY,
+						["note"] = ""
+					}
+				end
+			end
 		end
 	end
 	
 	-- Announce the links. Only do so once per GUID
 	-- FIXME Character limit could be a problem
-	if doAnnnounce and not(announcedGuids[guid]) then
+	if doAnnnounce and guid ~= nil and not(announcedGuids[guid]) and string.len(lootMessage) ~= 0 then
 		announcedGuids[guid] = true
-		SendChatMessage(lootMessage, channelToAnnounce);
+		SendChatMessage(lootMessage, CHANNEL_TO_ANNOUNCE);
 	end
 	
-	
+	if numItemsToMasterLoot >= 1 then 
+		showLootFrame(masterLootCandidates)
+	end
 end
 
+function showLootFrame(loot) 
+		HugeLoot:Print("Showing loot")
+	local lootFrame = AceGUI:Create("Frame")
+	lootFrame:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end)
+	lootFrame:SetTitle("Huge Loot")
+	local lootName = UnitName("target")
+	if lootName == nil then 
+		lootName = "Unknown"
+	end
+	lootFrame:SetStatusText("Currently looting "..lootName)
+	lootFrame:SetLayout("List")
+	
+	-- For each item, add a label, then a button for each step in prio, followed by the note.
+	for name, item in pairs(loot) do 
+		if item.priority == nil or string.len(item.priority) == 0 then 
+			item.priority = DEFAULT_PRIORITY
+		end
+	
+		local itemGroup = AceGUI:Create("SimpleGroup") 
+		itemGroup:SetFullWidth(true)
+		--itemGroup:SetHeight(100)
+		itemGroup:SetLayout("Flow")
+		
+		lootFrame:AddChild(itemGroup)
+				
+		local itemLabel = AceGUI:Create("Label") 
+		itemLabel:SetText(item.link)
+		itemLabel:SetWidth(150)
+		itemGroup:AddChild(itemLabel)
+		
+		local splitPriority = mySplit(item.priority, ">")
+		for i = 1, #splitPriority do
+			local line = splitPriority[i]
+			
+			local button = AceGUI:Create("Button") 
+			button:SetText(line)
+			button:SetWidth(150)
+			button:SetCallback("OnClick", function() SendChatMessage(item.link..line, CHANNEL_TO_MASTER_LOOT); end)
+			itemGroup:AddChild(button)
+		end
+		
+
+		local itemNote = AceGUI:Create("Label") 
+		itemNote:SetText(item.note)
+		itemGroup:AddChild(itemNote)
+	end
+end
+
+-- Blindly copied from stack overflow, review if it works poorly
 function mySplit(inputstr, sep)
-        if sep == nil then
-                sep = "%s"
-        end
-        local t={}
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-                table.insert(t, str)
-        end
-        return t
+	if sep == nil then
+		sep = "%s"
+	end
+	local t={}
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		table.insert(t, str)
+	end
+	return t
 end
