3475194671faaaca8370ebf5a56b95bc1d8372a3
Karl Beier <karl.beier@optus.com.au>
2020-02-28 11:38:32 +1100

    Improve UI and add initial loot tracking

--- HugeLoot.lua
+++ HugeLoot.lua
@@ -13,15 +13,23 @@ local ITEMS_TO_NOT_ANNOUNCE = {
 }
 local announcedGuids = {}
 local prioritySet = {}
+local isShowing = false
 
 HugeLoot:RegisterChatCommand("hgl", "ProcessCommand")
 
 -- Register for events
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("LOOT_OPENED") -- Fired on user opening a loot panel
+frame:RegisterEvent("CHAT_MSG_LOOT") -- Fired on someone receiving loot
 frame:SetScript("OnEvent", function(self, event, ...)
 	if event == "LOOT_OPENED" then 
 		processLoot()
+	elseif event == "CHAT_MSG_LOOT" then 
+		if not(isMasterLooter) then return end
+		message, two, _, _, receiver = ...
+		if string.find(message, "receive") and receiver ~= nil then
+			saveLootedItem(string.match(message, "%[(.*)"), receiver)
+		end
 	end
 end)
 
@@ -166,6 +174,36 @@ function parseLootPriority(input)
 	HugeLoot:Print("Successfully added "..itemsParsed.." items to the priority database")
 end
 
+-- Finds the class restriction for then item in the given loot slot, if it exists. Nil otherwise
+-- Inspired by https://wowwiki.fandom.com/wiki/UIOBJECT_GameTooltip
+local function findClassForSlot(slot) 
+	CreateFrame( "GameTooltip", "MyScanningTooltip", nil, "GameTooltipTemplate" ); -- Tooltip name cannot be nil
+	MyScanningTooltip:SetOwner( WorldFrame, "ANCHOR_NONE" );
+	-- Allow tooltip SetX() methods to dynamically add new lines based on these
+	MyScanningTooltip:AddFontStrings(
+		MyScanningTooltip:CreateFontString( "$parentTextLeft1", nil, "GameTooltipText" ),
+		MyScanningTooltip:CreateFontString( "$parentTextRight1", nil, "GameTooltipText" ) 
+	);
+	
+	MyScanningTooltip:ClearLines() 
+	MyScanningTooltip:SetLootItem(slot)
+	
+	local class = nil
+	for i = 1, MyScanningTooltip:NumLines() do
+		local mytext=_G["MyScanningTooltipTextLeft"..i] 
+		if mytext ~= nil then 
+			local status, text = pcall(function() return mytext:GetText() end)
+			if status and string.find(text, "Classes") then 
+				class = mySplit(text, " ")[2]
+			end
+		end
+	end
+	
+	MyScanningTooltip:Hide()
+	
+	return class
+end
+
 function processLoot() 
 	--HugeLoot:Print("Processing loot")
 	local masterlooterRaidID = select(3, GetLootMethod())
@@ -193,23 +231,23 @@ function processLoot()
 			numItemsToMasterLoot = numItemsToMasterLoot + 1
 			local priorityEntry = prioritySet[name]
 			
+			--FIXME duplicates
 			if priorityEntry ~= nil then 					
 				masterLootCandidates[name] = {
 					["link"] = link,
-					["priority"] = priorityEntry.priority.." > Open Roll",
+					["priority"] = priorityEntry.priority.." > Roll",
 					["note"] = priorityEntry.note,
 					["icon"] = icon
 				}
 			else
-				HugeLoot:Print("Stats for "..name)
-				local specTable = GetItemStats(link)
-				for k, v in pairs(specTable) do
-					HugeLoot:Print("Key: "..k.." | Value: "..v)
+				local class = findClassForSlot(i)
+				if class == "Warrior" then 
+					class = "Warr Tank > Fury"
 				end
-				if #specTable >= 1 then 
+				if class ~= nil then 
 					masterLootCandidates[name] = {
 						["link"] = link,
-						["priority"] = specTable[0],
+						["priority"] = class,
 						["note"] = "",
 						["icon"] = icon
 					}
@@ -237,9 +275,36 @@ function processLoot()
 	end
 end
 
+local function showRoll(item) 
+	if item.rollMonitor ~= nil then 
+		if item.maxRoll ~= 0 then 
+			item.rollMonitor:SetText(item.rolls[item.maxRoll].." - "..item.maxRoll)
+		else 
+			item.rollMonitor:SetText("None")
+		end
+	end
+end
+
+function saveLootedItem(item, name) 
+	local itemHistoryDB = HugeLoot.db.profile.lootHistory
+	if itemHistoryDB == nil then
+		itemHistoryDB = {}
+	end
+	itemHistoryDB[date("%y-%m-%d %H:%M:%S")] = {
+		["item"] = item,
+		["name"] = name
+	}
+	HugeLoot.db.profile.lootHistory = itemHistoryDB
+end
+
 function showLootFrame(loot) 
-	--HugeLoot:Print("Showing loot")
-	local currentItem = {}	
+	if isShowing then
+		return
+	else 
+		isShowing = true
+	end
+	
+	local currentItemName = nil
 	
 	local baseContainer = AceGUI:Create("Frame")
 	baseContainer:SetTitle("Huge Loot")
@@ -248,10 +313,8 @@ function showLootFrame(loot)
 		lootName = "Unknown"
 	end
 	baseContainer:SetStatusText("Currently looting "..lootName)
-	baseContainer:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
+	baseContainer:SetCallback("OnClose", function(widget) AceGUI:Release(widget) isShowing = false end)
 	baseContainer:SetLayout("Fill")
-
-	--baseContainer:SetAutoAdjustHeight(true)
 	
 	local lootFrame = AceGUI:Create("ScrollFrame")
 	lootFrame:SetLayout("List")
@@ -264,17 +327,17 @@ function showLootFrame(loot)
 			local text, playerName = ...
 			local splitText = mySplit(text, " ")
 			-- Only handle valid rolls of 1-100
-			if "rolls" == splitText[2] and "(1-100)" == splitText[4] and currentItem["rollMonitor"] ~= nil then
+			if "rolls" == splitText[2] and "(1-100)" == splitText[4] and loot[currentItemName]["rollMonitor"] ~= nil then
 				local newRoll = tonumber(splitText[3])
 				local playerName = splitText[1]
-				if newRoll > currentItem.maxRoll then
-					currentItem.rolls[newRoll] = playerName
-					currentItem.maxRoll = newRoll
-				elseif newRoll == currentItem.maxRoll then
-					currentItem.rolls[newRoll] = currentItem.rolls[newRoll].." + "..playerName
+				if newRoll > loot[currentItemName].maxRoll then
+					loot[currentItemName].rolls[newRoll] = playerName
+					loot[currentItemName].maxRoll = newRoll
+				elseif newRoll == loot[currentItemName].maxRoll then
+					loot[currentItemName].rolls[newRoll] = loot[currentItemName].rolls[newRoll].." + "..playerName
 				end
 				
-				currentItem.rollMonitor:SetText(currentItem.rolls[newRoll].." - "..newRoll)
+				showRoll(loot[currentItemName])
 			end
 		end
 	end)
@@ -302,28 +365,52 @@ function showLootFrame(loot)
 		itemLabel:SetWidth(130)
 		itemGroup:AddChild(itemLabel)
 		
+		local assignButton = AceGUI:Create("Button") 
+		assignButton:SetText("A")
+		assignButton:SetWidth(20)
+		assignButton:SetCallback("OnClick", function() 
+			itemGroup:Disable()
+		end)
+		itemGroup:AddChild(assignButton)
+				
+		local removeRollerButton = AceGUI:Create("Button") 
+		removeRollerButton:SetText("X")
+		removeRollerButton:SetWidth(20)
+		removeRollerButton:SetCallback("OnClick", function() 
+			item.rolls[item.maxRoll] = nil
+			local newMax = 0
+			for roll, name in pairs(item.rolls) do 
+				if roll > newMax and name ~= nil then
+					newMax = roll
+				end
+			end
+			item.maxRoll = newMax
+			showRoll(item)
+		end)
+		itemGroup:AddChild(removeRollerButton)
+		
 		local rollMonitor = AceGUI:Create("InteractiveLabel") 
 		rollMonitor:SetText("None")
 		rollMonitor:SetWidth(120)
 		itemGroup:AddChild(rollMonitor)
 		
+		item.rollMonitor = rollMonitor
+		item.rolls = {}
+		item.maxRoll = 0
+				
 		local splitPriority = mySplit(item.priority, ">")
 		for i = 1, #splitPriority do
-			local line = splitPriority[i]
+			local line = trim(splitPriority[i])
 			
 			local button = AceGUI:Create("Button") 
 			button:SetText(line)
 			button:SetWidth(135)
 			button:SetCallback("OnClick", function() 
-				currentItem = item
-				currentItem.rollMonitor = rollMonitor
-				currentItem.rolls = {}
-				currentItem.maxRoll = 0
-				SendChatMessage(item.link..line, CHANNEL_TO_MASTER_LOOT);
+				currentItemName = name				
+				SendChatMessage(item.link.." "..line, CHANNEL_TO_MASTER_LOOT);
 			end)
 			itemGroup:AddChild(button)
 		end
-		
 
 		local itemNote = AceGUI:Create("Label") 
 		itemNote:SetText(item.note)
@@ -333,6 +420,11 @@ end
 
 function processRoll(playerName, rollAmount) 
 	HugeLoot:Print("Received roll from "..playerName.." for value "..rollAmount)
+end
+
+-- Taken from http://lua-users.org/wiki/StringTrim
+function trim(s)
+   return s:match "^%s*(.-)%s*$"
 end
 
 -- Blindly copied from stack overflow, review if it works poorly
